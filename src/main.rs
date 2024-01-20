7c59ea41c2dda712c6c2b23fdc65369c7a3a683f
Iris Teyssier <iris.teyssier@gmail.com>
2024-01-19 22:46:44 -0800

    setup fast builds

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use bevy::prelude::*;
+use bevy_xpbd_2d::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Candy Count".into(),
+                resolution: (320., 180.).into(),
+                mode: Fullscreen,
+                resizable: false,
+                enabled_buttons: EnabledButtons {
+                    maximize: false,
+                    ..default()
+                },
+                ..default()
+            }),
+            ..default()
+        }))
+        .run();
 }
