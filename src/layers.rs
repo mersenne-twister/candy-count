1bb08288fbc6e6deda11e423fb7a0c27988be5fe
Iris Teyssier <iris.teyssier@gmail.com>
2024-01-21 09:58:18 -0800

    EXPLOSIVE MARBLES YAYYYY

--- src/layers.rs
+++ src/layers.rs
@@ -1,3 +1,3 @@
-pub const TABLE: f32 = 0.;
-
 pub const JAR: f32 = 5.;
+
+pub const MARBLES: f32 = 4.;
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod layers;
 
 fn main() {
     App::new()
-        .add_plugins(
+        .add_plugins((
             DefaultPlugins
                 .set(WindowPlugin {
                     primary_window: Some(Window {
@@ -26,7 +26,9 @@ fn main() {
                     ..default()
                 })
                 .set(ImagePlugin::default_nearest()),
-        )
+            PhysicsPlugins::default(),
+            // PhysicsDebugPlugin::default(),
+        ))
         .add_systems(Startup, setup)
         .run();
 }
@@ -34,21 +36,46 @@ fn main() {
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
 
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("jar.png"),
-        transform: Transform {
-            translation: Vec3::new(0., 5., layers::JAR),
-            ..default()
-        },
-        ..default()
-    });
+    //spawn the jar, with a collider
+    commands
+        .spawn((
+            SpriteBundle {
+                texture: asset_server.load("jar.png"),
+                transform: Transform {
+                    translation: Vec3::new(0., 5., layers::JAR),
+                    ..default()
+                },
+                ..default()
+            },
+            RigidBody::Static,
+            // Collider::Comp
+            // Collider::segment((-49., 50.).into(), (-49., -70.).into()),
+            // Collider::segment((-49., -65.).into(), (0., -70.).into()),
+            // Collider::segment((0., -70.).into(), (49., -65.).into()),
+            // Collider::segment((49., -70.).into(), (49., 50.).into())
+        ))
+        .with_children(|children| {
+            children.spawn(Collider::segment((-49., 50.).into(), (-49., -70.).into()));
+            children.spawn(Collider::segment((-49., -65.).into(), (0., -72.).into()));
+            children.spawn(Collider::segment((0., -72.).into(), (49., -65.).into()));
+            children.spawn(Collider::segment((49., -70.).into(), (49., 50.).into()));
+        });
 
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("table.png"),
-        transform: Transform {
-            translation: Vec3::new(0., 0., layers::TABLE),
-            ..default()
-        },
-        ..default()
-    });
+    for x in -10..10 {
+        for y in 80..100 {
+            commands.spawn((
+                SpriteBundle {
+                    texture: random_candy(&asset_server),
+                    transform: Transform::from_translation(Vec3::new(0., 50., layers::MARBLES)),
+                    ..default()
+                },
+                RigidBody::Dynamic,
+                Collider::ball(1.7),
+            ));
+        }
+    }
+}
+
+fn random_candy(asset_server: &Res<AssetServer>) -> Handle<Image> {
+    asset_server.load("candy1.png")
 }
